33cccac3bf268b8dfd48e4b73de392c65ffaf439
taha_f <84233310+madad-tech@users.noreply.github.com>
2023-04-07 21:27:10 +0000

    Update for function ssd1306_WriteBitmap

--- lib/ssd1306.h
+++ lib/ssd1306.h
@@ -70,6 +70,7 @@ void ssd1306_Fill(SSD1306_COLOR color);
 void ssd1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_COLOR color);
 char ssd1306_WriteChar(char ch, FontDef Font, SSD1306_COLOR color);
 char ssd1306_WriteString(const char* str, FontDef Font, SSD1306_COLOR color);
+void ssd1306_WriteBitmap(uint8_t *bmp, SSD1306_COLOR color);
 void ssd1306_SetCursor(uint8_t x, uint8_t y);
 void ssd1306_InvertColors(void);
 
